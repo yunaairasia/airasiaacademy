19de82057dfef4d2e2b8ca9ca526341e2f8111cd
yunaairasia <130623825+yunaairasia@users.noreply.github.com>
2023-04-13 12:22:28 +0800

    Update saleyuna.py

--- saleyuna.py
+++ saleyuna.py
@@ -25,7 +25,7 @@ df = user_input_features()
 st.subheader('User Input parameters')
 st.write(df)
 
-loaded_model = pickle.load(open("yuna_model.h5", "rb"))
+loaded_model = pickle.load(open("Saleyuna.h5", "rb"))
 
 prediction = loaded_model.predict(df)
 
